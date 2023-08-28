dc55b0b5f587f0a73b0012dc5ed984affe5539ac
lits-06 <minhanh0160@gmail.com>
2023-08-28 14:17:26 +0700

    update mongo atlas

--- app/main.py
+++ app/main.py
@@ -1,11 +1,23 @@
 from fastapi import FastAPI
-from pymongo import MongoClient
 
-client = MongoClient("mongodb://team1:123@mongodb:27017/")
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+uri = "mongodb+srv://admin:admin123@cluster0.ymqhm3k.mongodb.net/?retryWrites=true&w=majority"
+
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
 
 db = client["db"]
 
-scholarship = db["member"]
+collection_scholarship = db["scholarship"]
 
 app = FastAPI()
 
@@ -16,14 +28,14 @@ def read_root():
 @app.post("/users")
 def create_user(name: str | None = None, age: int | None = None):
     if name and age:
-        scholarship.insert_one({"name": name, "age": age})
+        collection_scholarship.insert_one({"name": name, "age": age})
         return "Update success"
     return "Lack of infomation"
 
 @app.get("/all")
 def all_user():
     projection = {"_id": False}
-    documents = list(scholarship.find({}, projection))
+    documents = list(collection_scholarship.find({}, projection))
     return {"users": documents}
 
 
