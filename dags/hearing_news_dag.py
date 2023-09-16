f49ced5cb515e31a3dde4c540d7d75fbf0f20c45
lits-06 <minhanh0160@gmail.com>
2023-09-17 02:53:25 +0700

    lit add hearing news

--- dags/hearing_news_dag.py
+++ dags/hearing_news_dag.py
@@ -3,6 +3,11 @@ import os
 import pendulum
 from airflow import DAG
 from airflow.decorators import task
+import requests
+import feedparser
+from datetime import datetime
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
 
 with DAG(
     dag_id="hearing_news",
@@ -12,7 +17,10 @@ with DAG(
     tags=["crawl_data"],
 ) as dag:
     
-    rss_links_file = "rss_links.txt"
+    uri = os.getenv("MONGO_URL")
+    client = MongoClient(uri, server_api=ServerApi('1'))
+    db = client["db"]
+    rss_collection = db["rss_links"]
     @task
     def get_source_urls():
         """
@@ -20,6 +28,10 @@ with DAG(
         Get rss feed urls from mongodb
         Output: return all urls 
         """
+        links = []
+        for document in rss_collection.find({}, {"_id", 0}):
+            links.append(document["link"])
+        return links
 
     @task 
     def check_new_posts(url: str):
@@ -27,6 +39,19 @@ with DAG(
             input: a url 
             output: all new schoolarship today url
         """
+        response = requests.get(url, verify=False)
+        feed = feedparser.parse(response.text)
+            if feed.bozo == 1:
+                print("some error")
+            for entry in feed.entries:
+                if not rss_collection.find_one({"title": entry.title}) and not rss_collection.find_one({"link": entry.link}) and "học bổng" in entry.title.lower():
+                    new_post = {
+                        "title": entry.title,
+                        "link": entry.link,
+                        "published": datetime.strptime(entry.published, "%a, %d %b %Y %H:%M:%S %z"),
+                    }
+                    rss_collection.insert_one(new_post)
+
 
     url = get_source_urls()
     check_new_posts.expand(url=url)
