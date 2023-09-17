c1d52a4fd377c7dcad6cb212d7d905f7c5958176
Nguyễn Hữu Nam Hòa <132972072+Codeximus2k4@users.noreply.github.com>
2023-09-17 10:32:08 +0700

    Add files via upload

--- task5.py
+++ task5.py
@@ -25,37 +25,39 @@ class Task5():
         tag_map={} 
         for a_tag in soup.find_all('a'):
              if a_tag.has_attr('href'):
-                  if a_tag.parent is None:
-                      continue
-             else:
-               parent = a_tag.parent
-               count=0
-               for each in parent.find_all('a'):
-                   count=count+1
-               if (count>2):
-                   continue  
-               tag= a_tag.parent
-               code=''
-               name  = tag.name
-               code = code+name
-               a_dict = tag.attrs
-               for each in list(a_dict.keys()):
-                   code= code + '+' + each  
-                   for one in a_dict[each]:
-                       code= code + ','+one 
-               if code not in list(tag_map.keys()):
-                   tag_map[code]=1
-               else :
-                   key_num = tag_map[code]+1
-                   tag_map[code]=key_num 
+                if a_tag.parent is None:
+                    continue
+                else:
+                    parent = a_tag.parent
+                    count=0
+                    for each in parent.find_all('a'):
+                        count=count+1
+                    if (count>2):
+                        continue  
+                    tag= a_tag.parent
+                    code=''
+                    name  = tag.name
+                    code = code+name
+                    a_dict = tag.attrs
+                    for each in list(a_dict.keys()):
+                        code= code + '+' + each  
+                        for one in a_dict[each]:
+                            code= code + ','+one 
+                    if code not in list(tag_map.keys()):
+                        tag_map[code]=1
+                    else :
+                        key_num = tag_map[code]+1
+                        tag_map[code]=key_num 
         max = 0     
         my_list = list(tag_map.keys())
+        max_tag='h3'
         for tags in my_list:
             if tags == my_list[0] or tags == my_list[-1]:
                 continue
             if tag_map[tags]>max:
                 max=tag_map[tags]
                 max_tag = tags
+        #print(tag_map)
         if ("uet" in url):
             key_tag= 'h3'
         else: 
@@ -76,7 +78,7 @@ class Task5():
                         if len(temp_list)>maxspace:
                             maxspace=len(temp_list)
                             key_tag = tags
-
+        #print(key_tag)
         #print(tag_map) 
         sections = key_tag.split("+")
         tag_name = sections[0]
@@ -94,7 +96,10 @@ class Task5():
                         continue
                     if each.a.has_attr('href'):
                         url = each.a['href']
-                        urls.append(url)
+                        if "https" not in url:
+                            url = "https://"+url
+                        if url not in urls:
+                            urls.append(url)
             return urls
         else:
             my_list = soup.find_all(tag_name,attrs = {attr_key[0]:attr_value[0]})
@@ -104,18 +109,21 @@ class Task5():
                     continue
                 if each.a.has_attr('href'):
                     url = each.a['href']
-                    urls.append(url)
+                    if "https" not in url:
+                            url = "https://"+url
+                    if url not in urls:
+                        urls.append(url)
             return urls           
         
-#instance = Task5.extract("https://uet.vnu.edu.vn/category/sinh-vien/hoc-phi-hoc-bong/")           
-    
+#instance = Task5.extract("https://www.iefa.org/scholarships?page=2")           
+#print(instance) 
 #extract = ExtractURL.extract_info("https://www.hust.edu.vn/vi/su-kien-noi-bat/hop-tac-doi-ngoai-truyen-thong/thong-bao-hoc-bong-erasmus-key-action-1-truong-dh-porto-bo-dao-nha-654608.html")
 #print(extract)
 
 
 
 app = FastAPI()
-@app.get("/task5/{full_path:path}")
+@app.get("/task5(path_type)/{full_path:path}")
 def task_5(full_path:str):
     result=[]
     result = result+ Task5.extract(full_path+"page/1")
@@ -123,5 +131,12 @@ def task_5(full_path:str):
         result = result + Task5.extract(full_path+ f"page/{i}")
     return {"result":result}
 
+@app.get("/task5(query_type)/{full_path:path}")
+def task_5(full_path:str):
+    result=[]
+    result = result+ Task5.extract(full_path+"?page=1")
+    for i in range(1,11):
+        result = result + Task5.extract(full_path+ f"?page={i}")
+    return {"result":result}
 
 
