3db5da08acf38f53516922a0457ead3bbcb2c478
Nguyễn Hữu Nam Hòa <132972072+Codeximus2k4@users.noreply.github.com>
2023-09-17 10:46:44 +0700

    Update api.py

--- app/router/api.py
+++ app/router/api.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from router import user, scholarship, scholarship_user, achievement, recommendation
+from router import user, scholarship, scholarship_user, achievement, recommendation,task5
 
 api_router = APIRouter()
 
@@ -9,3 +9,4 @@ api_router.include_router(scholarship.router, prefix = "/api", tags = ["scholars
 api_router.include_router(scholarship_user.router, prefix = "/api", tags = ["scholarship_user"]) 
 api_router.include_router(achievement.router, prefix = "/api", tags = ["achievement"]) 
 api_router.include_router(recommendation.router, prefix = "/api", tags = ["scholarship"]) 
+api_router.include_router(task5.router,prefix="/api",tags = ["task5"])
